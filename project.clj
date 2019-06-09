be33cad796db71f1d18f097374d4dc93572b38f7
Esko Lahti <esko.p.lahti@gmail.com>
2019-06-09 21:34:16 +0300

    Use stanga-secretary-bot-heroku for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stanga.secretary-bot "0.1.0"
+(defproject secretary-bot "0.1.0"
   :description "Stanga Secretary Bot"
   :url "https://github.com/stanga-racing/secretary-bot"
   :dependencies [[org.clojure/clojure "1.10.0"]
