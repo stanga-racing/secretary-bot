8b6d7b78e668bf3d5ee07219bde4e1ca7326ecf1
Esko Lahti <esko.p.lahti@gmail.com>
2019-06-10 08:17:12 +0300

    Don't use spec

--- src/clj/stanga/config.clj
+++ src/clj/stanga/config.clj
@@ -1,16 +1,5 @@
 (ns stanga.config
-  (:require [clojure.spec.alpha :as spec]
-            [environ.core :as e]))
-
-(spec/check-asserts true)
-
-(spec/def ::master-excel-api-key string?)
-(spec/def ::master-excel-spreadsheet-id string?)
-(spec/def ::general-webhook-url string?)
-
-(spec/def ::config (spec/keys :req-un [::master-excel-api-key
-                                       ::master-excel-spreadsheet-id
-                                       ::general-webhook-url]))
+  (:require [environ.core :as e]))
 
 (defn- make-database-config []
   (let [[_
@@ -31,7 +20,6 @@
      :port     port}))
 
 (defn config []
-  {:post [(spec/valid? ::config %)]}
   {:database                    (make-database-config)
    :general-webhook-url         (e/env :stanga-general-webhook-url)
    :master-excel-api-key        (e/env :stanga-master-excel-api-key)
